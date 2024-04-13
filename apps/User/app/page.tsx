b2879c8f5d6e55610bb969fea2e10d80e27c4e81
Madhavdhuria <madhavdhuria556@gmail.com>
2024-04-13 13:52:31 +0530

    Completed dashboard

--- apps/User/app/page.tsx
+++ apps/User/app/page.tsx
@@ -1,9 +1,13 @@
-"use client"
-import { AppbarClient } from "../components/AppbarClient";
-export default function Page(): JSX.Element {
-  return (
-   <div>
-    <AppbarClient/>
-   </div>
-  );
+import { getServerSession } from "next-auth";
+import { redirect } from 'next/navigation'
+import { authOptions } from "./lib/auth";
+
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect('/dashboard')
+  } else {
+    redirect('/api/auth/signin')
+  }
+  
 }
